24f9775f284551c78f19d120e571c46e00b0c36f
KMunyede <k.munyede@gmail.com>
2026-07-23 21:58:35 +0200

    seo: replace static robots.txt with dynamic robots.ts to block query parameter bloat

---
+++ apps/hilmost_toolbox_web/src/app/robots.ts
@@ -0,0 +1,22 @@
+import { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [
+          '/api/',
+          '/_next/',
+          '/cdn-cgi/',
+          '/*?val1=*',
+          '/*?unit1=*',
+          '/*?val2=*',
+          '/*?unit2=*',
+        ],
+      },
+    ],
+    sitemap: 'https://hilmost-toolbox.hilmost.net/sitemap.xml',
+  };
+}
